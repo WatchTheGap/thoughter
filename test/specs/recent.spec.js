93c89815643c477b6e0638b489dc87fb45747f20
Sara Basile <sarabasile@Saras-MacBook-Air.local>
2017-03-16 16:03:20 -0400

    added test to check element creation function

--- test/specs/recent.spec.js
+++ test/specs/recent.spec.js
@@ -9,12 +9,14 @@
     describe('showRecent tests', function () {
 
       beforeEach(function () {
-        document.querySelector('body').appendChild(document.createElement('main').addClass('.recent'));
+        let newMain = document.createElement('main');
+        newMain.classList.add('recent');
+        document.querySelector('body').appendChild(newMain);
       });
 
-      afterEach();
-        let newMain = document.querySelector('main');
-        newMain.parentNode.removeChild(newMain);
+      // afterEach();
+      //   let newMain = document.querySelector('main');
+      //   newMain.parentNode.removeChild(newMain);
 
 
       it('should show a list of elements containing data from the provided array', function () {
@@ -31,8 +33,11 @@
         let result = window.thoughter.showRecent('SAD');
         expect(result).to.be.undefined;
       });
-
-
+      it('should create an element for each piece of data', function () {
+        window.thoughter.showRecent([{content: 'testing', createTime: 'time', id: 'id'}]);
+        let articles = document.querySelectorAll('main article');
+        expect(articles.length).to.equal(1);
+      });
     });
 
     describe('getRecent', function () {
