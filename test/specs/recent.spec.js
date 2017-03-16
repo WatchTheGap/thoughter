5627430ebecdfee0084e7bfacee32331d8dc04ea
Sara Basile <sarabasile@Saras-MacBook-Air.local>
2017-03-16 13:50:20 -0400

    set up empty describe blocks in spec file

---
+++ test/specs/recent.spec.js
@@ -0,0 +1,21 @@
+(function() {
+  'use strict';
+
+
+  let expect = expect.chai;
+
+  describe('recent thoughts tests', function () {
+
+    describe('showRecent tests', function () {
+
+
+    });
+
+    describe('getRecent', function () {
+
+    });
+
+  });
+
+
+}());
